c87f6431a8b6acf3cc4b83143f80d9a8f5edf7b9
Matthew Black <mblackmblackmblack@gmail.com>
2019-09-26 16:13:08 -0400

    Kill jobs endpoint

---
+++ test/loan/lender/deploy/jobs.js
@@ -0,0 +1,28 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const chaiAsPromised = require('chai-as-promised')
+
+const { cancelJobs } = require('../../loanCommon')
+
+chai.should()
+const expect = chai.expect
+
+chai.use(chaiHttp)
+chai.use(chaiAsPromised)
+
+const lenderServer = 'http://localhost:3030/api/loan'
+const arbiterServer = 'http://localhost:3032/api/loan'
+
+function stopJobs (web3Chain) {
+  describe('Stop Jobs', () => {
+    it('Should stop jobs for lender and arbiter agents', async () => {
+      await cancelJobs(lenderServer)
+      await cancelJobs(arbiterServer)
+    })
+  })
+}
+
+describe('Agent Jobs', () => {
+  stopJobs()
+})
