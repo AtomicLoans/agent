1c4ee677d4d7c3ea9286f9c2be77b75f591ad840
Matthew Black <mblackmblackmblack@gmail.com>
2019-10-07 15:33:29 -0400

    Improve collateral claim checking

--- src/worker/loan/sales/claim.js
+++ src/worker/loan/sales/claim.js
@@ -8,6 +8,7 @@ function defineSalesClaimJobs (agenda) {
 
     const { data } = job.attrs
     const { saleModelId } = data
+    const { NETWORK } = process.env
 
     const sale = await Sale.findOne({ _id: saleModelId }).exec()
     if (!sale) return console.log('Error: Sale not found')
@@ -15,35 +16,53 @@ function defineSalesClaimJobs (agenda) {
 
     console.log('initTxHash', initTxHash)
 
-    const collateralBlockHeight = await sale.collateralClient().chain.getBlockHeight()
-    const { latestCollateralBlock } = sale
-    let curBlock = latestCollateralBlock + 1
+    if (NETWORK === 'mainnet' || NETWORK === 'kovan') {
+      let baseUrl
+      if (NETWORK === 'mainnet') {
+        const baseUrl = 'https://blockstream.info'
+      } else {
+        const baseUrl = 'https://blockstream.info/testnet'
+      }
+      const { status, data: spendInfo } = await axios.get(`${baseUrl}/api/tx/${initTxHash}/outspend/0`)
 
-    while (curBlock <= collateralBlockHeight) {
-      const block = await sale.collateralClient().chain.getBlockByNumber(curBlock)
-      const txs = await Promise.all(block.transactions.map((txid) => {
-        return sale.collateralClient().getMethod('getTransactionByHash')(txid)
-      }))
-
-      for (let i = 0; i < txs.length; i++) {
-        const tx = txs[i]
-        const vins = tx._raw.vin
-        for (let j = 0; j < vins.length; j++) {
-          const vin = vins[j]
-          if (vin.txid === initTxHash) {
-            console.log('COLLATERAL_CLAIMED FOUND')
-            sale.claimTxHash = tx.hash
-            sale.status = 'COLLATERAL_CLAIMED'
-            curBlock = collateralBlockHeight + 1
-            break
-          }
+      if (status === 200) {
+        if (spendInfo.spent === true) {
+          console.log('COLLATERAL_CLAIMED FOUND')
+          sale.claimTxHash = spendInfo.txid
+          sale.status = 'COLLATERAL_CLAIMED'
         }
       }
+    } else {
+      const collateralBlockHeight = await sale.collateralClient().chain.getBlockHeight()
+      const { latestCollateralBlock } = sale
+      let curBlock = latestCollateralBlock + 1
 
-      curBlock++
+      while (curBlock <= collateralBlockHeight) {
+        const block = await sale.collateralClient().chain.getBlockByNumber(curBlock)
+        const txs = await Promise.all(block.transactions.map((txid) => {
+          return sale.collateralClient().getMethod('getTransactionByHash')(txid)
+        }))
+
+        for (let i = 0; i < txs.length; i++) {
+          const tx = txs[i]
+          const vins = tx._raw.vin
+          for (let j = 0; j < vins.length; j++) {
+            const vin = vins[j]
+            if (vin.txid === initTxHash) {
+              console.log('COLLATERAL_CLAIMED FOUND')
+              sale.claimTxHash = tx.hash
+              sale.status = 'COLLATERAL_CLAIMED'
+              curBlock = collateralBlockHeight + 1
+              break
+            }
+          }
+        }
+
+        curBlock++
+      }
+
+      sale.latestCollateralBlock = collateralBlockHeight
     }
-
-    sale.latestCollateralBlock = collateralBlockHeight
 
     await sale.save()
 
