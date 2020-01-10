2fc762e63c90a2e05bbcbb9655b72bce27c5aa4a
Matthew Black <mblackmblackmblack@gmail.com>
2020-01-10 11:49:23 -0500

    Add details for generating mnemonic in travis test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const { MONGOOSE_DEBUG, MONGODB_ARBITER_URI, MONGODB_URI, HEROKU_APP } = process.env
-const { isArbiter } = require('./utils/env')
+const { MONGOOSE_DEBUG, MONGODB_ARBITER_URI, MONGODB_URI, HEROKU_APP, NODE_ENV, MNEMONIC, MNEMONIC_ARBITER, PARTY } = process.env
+
+const { isArbiter, rewriteEnv } = require('./utils/env')
 const mongoose = require('mongoose')
+const { generateMnemonic } = require('bip39')
 
 if (MONGOOSE_DEBUG === 'true') {
   mongoose.set('debug', true)
@@ -21,8 +23,24 @@ async function start() {
       const mnemonic = mnemonics[0]
       process.env.MNEMONIC = mnemonic.mnemonic
     } else {
-      const mnemonic = new Mnemonic({ mnemonic: process.env.MNEMONIC })
+      const mnemonic = new Mnemonic({ mnemonic: MNEMONIC })
       await mnemonic.save()
+    }
+  }
+
+  if (NODE_ENV === 'test') {
+    if (PARTY === 'arbiter') {
+      if (MNEMONIC_ARBITER === 'undefined' || MNEMONIC_ARBITER === undefined || MNEMONIC_ARBITER === '') {
+        const mnemonic = generateMnemonic(128)
+        rewriteEnv('.env', 'MNEMONIC_ARBITER', `"${mnemonic}"`)
+        process.env.MNEMONIC_ARBITER = mnemonic
+      }
+    } else if (PARTY === 'lender') {
+      if (MNEMONIC === 'undefined' || MNEMONIC === undefined || MNEMONIC === '') {
+        const mnemonic = generateMnemonic(128)
+        rewriteEnv('.env', 'MNEMONIC', `"${mnemonic}"`)
+        process.env.MNEMONIC = mnemonic
+      }
     }
   }
 
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
 function isArbiter () {
   return process.env.PARTY === 'arbiter'
 }
 
+function rewriteEnv (envFile, key, value) {
+  if (fs.existsSync(path.resolve(process.cwd(), envFile))) {
+    const env = fs.readFileSync(path.resolve(process.cwd(), envFile), 'utf-8')
+    const regex = new RegExp(`${key}=("(.*?)"|([0-9a-zA-Z])\\w+)`, 'g')
+    const newEnv = env.replace(regex, `${key}=${value}`)
+    fs.writeFileSync(path.resolve(process.cwd(), envFile), newEnv, 'utf-8')
+  } else {
+    const newEnv = `${key}=${value}`
+    fs.writeFileSync(path.resolve(process.cwd(), envFile), newEnv, 'utf-8')
+  }
+}
+
 module.exports = {
-  isArbiter
+  isArbiter,
+  rewriteEnv
 }
