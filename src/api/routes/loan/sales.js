0bde14066b28e93727e6a26639156c1008cb6978
Steven Zhao <steven@zhao.io>
2020-03-09 14:55:58 -0400

    Fix liquidation

--- src/api/routes/loan/sales.js
+++ src/api/routes/loan/sales.js
@@ -10,11 +10,7 @@ function defineSalesRouter (router) {
 
   router.get('/sales/contract/:principal/:saleId', asyncHandler(async (req, res, next) => {
     const { params } = req
-    let { principal, saleId } = params
-
-    if (principal === 'DAI') {
-      principal = 'SAI'
-    }
+    const { principal, saleId } = params
 
     const sale = await Sale.findOne({ principal, saleId }).exec()
     if (!sale) return next(res.createError(401, 'Sale not found'))
